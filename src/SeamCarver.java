9b408bbdd45d4745de3e8bf4f77f61b7bddc75bb
nick serebrennikov <Nick.Serebrennikov@derivco.co.uk>
2017-11-13 20:41:37 +0000

    calculate energy and check with tests

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -6,7 +6,7 @@ public class SeamCarver {
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
-        this.picture = picture;
+        this.picture = new Picture(picture);//create defensive copy
     }
 
     // current picture
@@ -26,7 +26,44 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        return 0.0d;
+        checkRange(x, y);
+        if (x == 0 || x == picture.width() - 1 || y == 0 || y == picture.height() - 1) {
+            return 1000;
+        }
+
+        double deltaX2 = Math.pow(Rx(x, y), 2) + Math.pow(Gx(x, y), 2) + Math.pow(Bx(x, y), 2);
+        double deltaY2 = Math.pow(Ry(x, y), 2) + Math.pow(Gy(x, y), 2) + Math.pow(By(x, y), 2);
+        return Math.sqrt(deltaX2 + deltaY2);
+    }
+
+    private int By(int x, int y) {
+        return picture.get(x, y + 1).getBlue() - picture.get(x, y - 1).getBlue();
+    }
+
+    private int Gy(int x, int y) {
+        return picture.get(x, y + 1).getGreen() - picture.get(x, y - 1).getGreen();
+    }
+
+    private int Ry(int x, int y) {
+        return picture.get(x, y + 1).getRed() - picture.get(x, y - 1).getRed();
+    }
+
+    private int Bx(int x, int y) {
+        return picture.get(x + 1, y).getBlue() - picture.get(x - 1, y).getBlue();
+    }
+
+    private int Gx(int x, int y) {
+        return picture.get(x + 1, y).getGreen() - picture.get(x - 1, y).getGreen();
+    }
+
+    private int Rx(int x, int y) {
+        return picture.get(x + 1, y).getRed() - picture.get(x - 1, y).getRed();
+    }
+
+    private void checkRange(int x, int y) {
+        if (x < 0 || x > picture.width() || y < 0 || y > picture.height()) {
+            throw new IllegalArgumentException();
+        }
     }
 
     // sequence of indices for horizontal seam
@@ -41,9 +78,35 @@ public class SeamCarver {
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
+        validateImage();
+        validateSeam(seam);
+
     }
+
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+        validateImage();
+        validateSeam(seam);
     }
+
+    private void validateImage() {
+        if (picture.height() <= 1 || picture.width() <= 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateSeam(int[] seam) {
+        if (seam == null) {
+            throw new IllegalArgumentException();
+        }
+        int seamItem = seam[0];
+        for (int s : seam) {
+            if (Math.abs(s - seamItem) > 1) {
+                throw new IllegalArgumentException();
+            }
+            seamItem = s;
+        }
+    }
+
 }
--- tests/SeamCarverTest.java
+++ tests/SeamCarverTest.java
@@ -1,6 +1,9 @@
+import edu.princeton.cs.algs4.Picture;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
 
 public class SeamCarverTest {
     @Rule
@@ -12,4 +15,31 @@ public class SeamCarverTest {
         new SeamCarver(null);
     }
 
+    @Test
+    public void CalculateEnergy_boreders() {
+        Picture picture = new Picture("3x4.png");
+        SeamCarver carver = new SeamCarver(picture);
+
+        assertEquals(1000, carver.energy(0, 0), 0.01);
+        assertEquals(1000, carver.energy(0, 1), 0.01);
+        assertEquals(1000, carver.energy(0, 2), 0.01);
+        assertEquals(1000, carver.energy(0, 3), 0.01);
+
+        assertEquals(1000, carver.energy(1, 0), 0.01);
+        assertEquals(1000, carver.energy(1, 3), 0.01);
+
+        assertEquals(1000, carver.energy(2, 0), 0.01);
+        assertEquals(1000, carver.energy(2, 1), 0.01);
+        assertEquals(1000, carver.energy(2, 2), 0.01);
+        assertEquals(1000, carver.energy(2, 3), 0.01);
+    }
+
+    @Test
+    public void CalculateEnergy() {
+        Picture picture = new Picture("3x4.png");
+        SeamCarver carver = new SeamCarver(picture);
+
+        assertEquals(Math.sqrt(52024), carver.energy(1, 2), 0.01);
+        assertEquals(Math.sqrt(52225), carver.energy(1, 1), 0.01);
+    }
 }
